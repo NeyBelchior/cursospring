fdfc6e8298fdb4f47a2a9a5c52766cda7712e325
Ney Belchior <neybelchior@nomus.com.br>
2022-01-11 09:12:26 -0300

    Criando Repository e service para categoria

---
+++ src/main/java/com/ney/cursomvc/repositories/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package com.ney.cursomvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ney.cursomvc.domain.Categoria;
+
+@Repository
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+	
+
+	
+	
+}
