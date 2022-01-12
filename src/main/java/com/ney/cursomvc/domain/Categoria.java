68550c10975a0c6264b9d4969a5bd5ec835b0014
Ney Belchior <neybelchior@nomus.com.br>
2022-01-11 23:14:47 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/ney/cursomvc/domain/Categoria.java
+++ src/main/java/com/ney/cursomvc/domain/Categoria.java
@@ -1,12 +1,17 @@
 package com.ney.cursomvc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 @Entity
 public class Categoria implements Serializable {
 	
@@ -16,6 +21,12 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)	
 	private Integer id;
 	private String nome;
+	
+
+	
+	
+	@ManyToMany(mappedBy ="categorias")		
+	private List<Produto> produtos= new ArrayList<>();	
 	
 	public Categoria() {
 		
@@ -43,6 +54,14 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -59,7 +78,8 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
+
 	
 
 }
---
+++ src/main/java/com/ney/cursomvc/repositories/ProdutoRepository.java
@@ -0,0 +1,18 @@
+package com.ney.cursomvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ney.cursomvc.domain.Categoria;
+import com.ney.cursomvc.domain.Produto;
+
+@Repository
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	
+	
+
+	
+	
+}
